0466a6e854bc27509b71447ca9732de384e40a65
vijay-cmtai <vijay.maurya@completrix.com>
2025-06-16 11:55:50 +0530

    Add files via upload

---
+++ src/sidemap.ts
@@ -0,0 +1,10 @@
+export const productSitemap = [
+  "/products/lexprate-cr",
+  "/products/calvilux",
+  "/products/multilux",
+  "/products/neurolux",
+  "/products/cardiolux",
+  "/products/diabetlux",
+  "/products/immunolux",
+  "/products/gastrolux",
+];
